3628bdeb2f6f8347b8b8bc85fcfa9c9babce3e8f
ootidea <ootidea@gmail.com>
2024-09-29 13:40:29 +0900

    chore: update dev env

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,8 @@
-import postcssOKLabFunction from '@csstools/postcss-oklab-function'
-import autoprefixer from 'autoprefixer'
 import { defineConfig } from 'vite'
 import solidPlugin from 'vite-plugin-solid'
 
 export default defineConfig({
   plugins: [solidPlugin()],
-  build: {
-    target: 'esnext',
-  },
-  server: {
-    port: 51591,
-  },
-  css: {
-    postcss: {
-      plugins: [autoprefixer(), postcssOKLabFunction({ subFeatures: { displayP3: false } })],
-    },
-  },
+  server: { port: 51591 },
+  resolve: { alias: { '~': '/src' } },
 })
